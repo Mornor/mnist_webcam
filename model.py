97d9883afb0df8ea01b17fb71b23585e4cd42e2a
Celien Nanson <cesliens@gmail.com>
2018-04-15 15:26:13 +0200

    Add loading and visualisation functions

---
+++ model.py
@@ -0,0 +1,9 @@
+import utils
+
+def split_dataset():
+    print('TODO')
+
+images, labels = utils.load_dataset()
+
+utils.display_image(images, 234)
+utils.display_label(labels, 234)
--- utils.py
+++ utils.py
@@ -1,6 +1,26 @@
 # Set of tools to perform operations on dataset
 # @author Celien Nanson <cesliens@gmail.com>
 
-def load_dataset:
-    print('TODO')
+# Import
+import mnist
+
+DATA_DIR_PATH = 'data/'
+mndata = mnist.MNIST(DATA_DIR_PATH)
+
+# Load and return the MNIST dataset
+# 60000 images and labels
+# Each image is 28x28 pixels
+def load_dataset():
+    images, labels = mndata.load_training()
+    return images, labels
+
+# Display the images[index] to the console
+def display_image(images, index):
+    print(mndata.display(images[index]))
+
+def display_label(labels, index):
+    print(labels[index])
+
+#images, labels = load_dataset()
+#display_image(images, 235)
 
