fd3bdc50c8253c034a8d5f0f2837ba66214554e6
Celien Nanson <cesliens@gmail.com>
2018-05-22 20:55:52 +0200

    [WIP] - Predict based on example image

--- predict.py
+++ predict.py
@@ -3,6 +3,7 @@ import keras
 from keras.optimizers import Adam
 from keras.models import model_from_json
 from keras.optimizers import Adadelta, Adam
+from PIL import Image
 
 def get_model():
     with open('model.json', 'r') as jfile:
@@ -13,5 +14,13 @@ def get_model():
     model.load_weights('model.h5')
     return model
 
+
+def predict_input(model, image):
+    preditcted_number = int(model.predict(image, batch_size=1))
+
 # Load the trained model
 model = get_model()
+
+# Predict output based on image
+image = Image.open("data/3.png")
+predicted_number = predict_input(model, image)
