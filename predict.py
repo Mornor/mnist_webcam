0ec03eba32466c33daa1a7badff6f61ed3dd77cd
Celien Nanson <cesliens@gmail.com>
2018-05-24 20:16:50 +0200

    Use cv2 to read test image, tranform test image to mnist format

--- predict.py
+++ predict.py
@@ -3,7 +3,7 @@ import keras
 from keras.optimizers import Adam
 from keras.models import model_from_json
 from keras.optimizers import Adadelta, Adam
-from PIL import Image
+import cv2
 
 def get_model():
     with open('model.json', 'r') as jfile:
@@ -14,6 +14,11 @@ def get_model():
     model.load_weights('model.h5')
     return model
 
+def img_to_mnist(image):
+    gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray_img = cv2.GaussianBlur(gray_img, (5, 5), 0)
+    gray_img = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, blockSize=321, C=28)
+    return gray_img
 
 def predict_input(model, image):
     preditcted_number = int(model.predict(image, batch_size=1))
@@ -22,5 +27,6 @@ def predict_input(model, image):
 model = get_model()
 
 # Predict output based on image
-image = Image.open("data/3.png")
+image = cv2.imread("data/3.png")
+image = img_to_mnist(image)
 predicted_number = predict_input(model, image)
